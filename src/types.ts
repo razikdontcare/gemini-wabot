58a163f779ca0093caa9abb82841c1f25b104ae0
razikdontcare <gamersia4@gmail.com>
2024-11-02 05:21:58 +0800

    Add detailed documentation for HandlerParams interface in types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,36 @@
 import makeWASocket, { type WAMessage, type MessageUpsertType } from "baileys";
 
+/**
+ * Interface representing the parameters passed to a handler.
+ */
 export interface HandlerParams {
+  /**
+   * The WebSocket connection instance created by `makeWASocket`.
+   */
   socket: ReturnType<typeof makeWASocket>;
+
+  /**
+   * An object containing message details.
+   */
   m: {
+    /**
+     * An array of WhatsApp messages.
+     */
     messages: WAMessage[];
+
+    /**
+     * The type of message upsert event.
+     */
     type: MessageUpsertType;
+
+    /**
+     * An optional request ID associated with the message.
+     */
     requestId?: string;
   };
+
+  /**
+   * The name of the session.
+   */
   sessionName: string;
 }
